244f69133831481ad087bc82f78f7f9f090125ac
Cristobal Caro <cacaro@uc.cl>
2024-04-29 21:08:27 +0200

    fetch function added to index.js

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+import { createCharacterCard } from "./components/card/card.js";
+
 const cardContainer = document.querySelector('[data-js="card-container"]');
 const searchBarContainer = document.querySelector(
   '[data-js="search-bar-container"]'
@@ -12,3 +14,21 @@ const pagination = document.querySelector('[data-js="pagination"]');
 const maxPage = 1;
 const page = 1;
 const searchQuery = "";
+
+async function fetchCharacters() {
+  const response = await fetch("https://rickandmortyapi.com/api/character");
+  console.log(response);
+  if (response.ok) {
+    const data = await response.json();
+    const characters = data.results;
+    for (let character of characters) {
+      createCharacterCard(
+        character.name,
+        character.type,
+        character.status,
+        character.episode.length
+      );
+    }
+  }
+}
+console.log(fetchCharacters());
