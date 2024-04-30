562af1d728620d8cfca0a3f48dbe57b18726145e
Camillo Hochberg <camillo.h@hotmail.de>
2024-04-30 12:26:31 +0200

    add searchbar function and implement in index.js

--- components/search-bar/search-bar.js
+++ components/search-bar/search-bar.js
@@ -1,12 +1,10 @@
-import { createCharacterCard } from "../card/card.js";
+// import { createCharacterCard } from "../card/card.js";
 
-export function searchFunction(fetchedData, searchInput) {
-  const result = fetchedData.find(({ name }) => name === searchInput);
-  const image = result.image;
-  const characterName = result.name;
-  const status = result.status;
-  const type = result.type;
-  const occurrences = result.episode.length;
-
-  createCharacterCard(image, characterName, status, type, occurrences);
-}
+// export function searchFunction(fetchedData, searchInput) {
+//   const result = fetchedData.filter((character) => {
+//     return character.name.startsWith(searchInput);
+//   });
+//   if (result) {
+//     createCharacterCard();
+//   }
+// }
